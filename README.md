264ebfe79a7824019d68c5e179778183aa8af632
StarTux <ths.loy@googlemail.com>
2020-07-07 00:32:19 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# InvisibleItemFrames
+
+To turn an item frame invisible, throw an invsibility potion at it, or hit it with an invisibility arrow. A direct hit is required.
+
+As a player, you require:
+- The `invisibleitemframes.use` permission node
+- Build permissions via the GenericEvents plugin
+
+## Dependencies
+
+- GenericEvents
---
+++ src/main/java/com/cavetale/invisibleitemframes/InvisibleItemFramesPlugin.java
@@ -0,0 +1,67 @@
+package com.cavetale.invisibleitemframes;
+
+import com.destroystokyo.paper.event.entity.ProjectileCollideEvent;
+import com.winthier.generic_events.GenericEvents;
+import org.bukkit.entity.Arrow;
+import org.bukkit.entity.ItemFrame;
+import org.bukkit.entity.Player;
+import org.bukkit.entity.Projectile;
+import org.bukkit.entity.ThrownPotion;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.potion.PotionData;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
+import org.bukkit.potion.PotionType;
+
+public final class InvisibleItemFramesPlugin extends JavaPlugin implements Listener {
+    @Override
+    public void onEnable() {
+        getServer().getPluginManager().registerEvents(this, this);
+    }
+
+    @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
+    void onProjectileCollide(ProjectileCollideEvent event) {
+        if (!(event.getCollidedWith() instanceof ItemFrame)) return;
+        ItemFrame itemFrame = (ItemFrame) event.getCollidedWith();
+        Projectile proj = event.getEntity();
+        // Check player
+        if (!(proj.getShooter() instanceof Player)) return;
+        Player player = (Player) proj.getShooter();
+        if (!player.hasPermission("invisibleitemframes.use")) return;
+        // Check projectile
+        if (proj instanceof Arrow) {
+            Arrow arrow = (Arrow) proj;
+            PotionData pot = arrow.getBasePotionData();
+            if (pot.getType() != PotionType.INVISIBILITY) return;
+        } else if (proj instanceof ThrownPotion) {
+            ThrownPotion thrown = (ThrownPotion) proj;
+            boolean hasInvisibility = false;
+            for (PotionEffect pot : thrown.getEffects()) {
+                if (PotionEffectType.INVISIBILITY.equals(pot.getType())) {
+                    hasInvisibility = true;
+                    break;
+                }
+            }
+            if (!hasInvisibility) return;
+        } else {
+            return;
+        }
+        // Check build perms
+        if (!GenericEvents.playerCanBuild(player, itemFrame.getLocation().getBlock())) return;
+        itemFrame.setVisible(false);
+        if (proj instanceof Arrow) event.setCancelled(true);
+    }
+
+    @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
+    void onEntityDamageByEntity(EntityDamageByEntityEvent event) {
+        if (!(event.getEntity() instanceof ItemFrame)) return;
+        if (!(event.getDamager() instanceof Player)) return;
+        ItemFrame itemFrame = (ItemFrame) event.getEntity();
+        if (itemFrame.isVisible()) return;
+        itemFrame.setVisible(true);
+    }
+}
